23db993a8bccf4ae183b9b15607bcd7cf8a1a4db
David Chu <dabidjohn424@gmail.com>
2026-03-04 17:49:15 +0800

    feat: on drag end event

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,19 @@ const App = () => {
 
   const dragItemIndex = useRef<number | null>(null);
 
-  const handleDragStart = (index: number) => {
+  const handleDragStart = (e: React.DragEvent, index: number) => {
     dragItemIndex.current = index;
+
+    const img = new Image();
+    img.src =
+      "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+
+    e.dataTransfer.setDragImage(img, 0, 0);
+    e.currentTarget.classList.add("opacity-50");
+  };
+
+  const handleDragEnd = (e: React.DragEvent) => {
+    e.currentTarget.classList.remove("opacity-50");
   };
 
   const handleDragEnter = (targetIndex: number) => {
@@ -31,9 +42,10 @@ const App = () => {
         <div
           key={item}
           draggable
-          onDragStart={() => handleDragStart(index)}
+          onDragStart={(e) => handleDragStart(e, index)}
           onDragEnter={() => handleDragEnter(index)}
           onDragOver={(e) => e.preventDefault()}
+          onDragEnd={(e) => handleDragEnd(e)}
           className="p-4 bg-blue-500 text-white cursor-move rounded shadow-md"
         >
           {item}
